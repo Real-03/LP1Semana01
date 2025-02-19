b38f6cf68fc2280792250fc432b2e7c3c3480b76
Tomás Real <tomasreal.art3d@gmail.com>
2025-02-19 18:40:23 +0000

    Add concatnacao and interpolacao

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -11,6 +11,12 @@ namespace ManyStrings
             string  text2 = "C:\t Este texto tem um tab";
             Console.WriteLine(text);
             Console.WriteLine(text2);
+
+
+            string concatnacao = "Isto é fixe "+3;
+            string interpolacao = $"valor de x e’ {text}";
+            Console.WriteLine(concatnacao);
+            Console.WriteLine(interpolacao);
         }
     }
 }
