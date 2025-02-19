466d2ff789ec5c435674e794642b261c60cbe575
Tomás Real <tomasreal.art3d@gmail.com>
2025-02-19 18:28:24 +0000

    Remove float from integer variables and ADD float and decimal

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -1,17 +1,31 @@
 ﻿using System;
-
+using System.Text;
 namespace VariousTypes
 {
     public class Program
     {
         private static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
             int HP = 100;
-            float RealNumber = 3.634f;
+            uint Number = 23U;
 
             Console.WriteLine(HP);
+            Console.WriteLine(Number);
+
+            char FirstChar = '\u0061';
+            char airplaneIcon = '\u2708';
+            char sun ='\u2600';
+
+            Console.WriteLine(FirstChar);
+            Console.WriteLine(airplaneIcon);
+            Console.WriteLine(sun);
+
+            float RealNumber = 3.634f;
+            decimal DecimalNumber = 3.42478753235m;
             Console.WriteLine(RealNumber);
-            
+            Console.WriteLine(DecimalNumber);
+
         }
     }
 }
