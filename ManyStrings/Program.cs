d806c1997bb4e4448db6ae590cafbf6c99bc130d
Tomás Real <tomasreal.art3d@gmail.com>
2025-02-19 18:45:50 +0000

    Add Formatted strings.

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
-
+using System.Text;
 namespace ManyStrings
 {
     public class Program
     {
         private static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            double xx = 1.23456;
+            int ii = 19;
+
 
             string  text = "C:\\Teste\\Teste";
             string  text2 = "C:\t Este texto tem um tab";
@@ -17,6 +21,12 @@ namespace ManyStrings
             string interpolacao = $"valor de x e’ {text}";
             Console.WriteLine(concatnacao);
             Console.WriteLine(interpolacao);
+
+            Console.WriteLine($"{xx:f2} e {xx:p}");
+            Console.WriteLine($"{ii:x} e {ii:c}");
+
+
+
         }
     }
 }
