c906836f439eb25e76a9243001fdd2b7bbf989fd
Tomás Real <tomasreal.art3d@gmail.com>
2025-02-19 18:10:45 +0000

    Add Function Description (///summary)

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -4,6 +4,10 @@ namespace MeuPrograma
 {
     public class Program
     {
+        /// <summary>
+        /// Function to print Hello LP1 and Bye World
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
