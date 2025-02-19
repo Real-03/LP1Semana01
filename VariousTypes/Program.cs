2e7fb928e7778c2a962d61cce66e96516830ca81
Tomás Real <tomasreal.art3d@gmail.com>
2025-02-19 18:17:34 +0000

    New Project and Integer variables with appropriate literals

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int HP = 100;
+            float RealNumber = 3.634f;
+
+            Console.WriteLine(HP);
+            Console.WriteLine(RealNumber);
+        }
+    }
+}
